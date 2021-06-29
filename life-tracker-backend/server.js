abd3a0e58839707a22dfc7ac13e26db1935f3a03
Nicholas de Souza <nrdesouzag13@gmail.com>
2021-06-29 09:51:11 -0700

    set up env variables and config for project

--- life-tracker-backend/server.js
+++ life-tracker-backend/server.js
@@ -2,6 +2,7 @@ const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
 const {BadRequestError, NotFoundError} = require("./utils/error")
+const { PORT } = require("./config")
 
 const app = express()
 
@@ -26,7 +27,6 @@ app.use((err, req,res,next) => {
         
 })
 
-const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
     console.log(`Server running on PORT ${PORT}`)
