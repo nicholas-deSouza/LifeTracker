5e52a1d0246754dc151e90c01e5be5e95953c735
Nicholas de Souza <nrdesouzag13@gmail.com>
2021-06-29 09:09:37 -0700

    server up

---
+++ life-tracker-backend/server.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+const morgan = require("morgan")
+
+const app = express()
+
+
+app.use(cors())
+
+app.use(express.json())
+
+app.use(morgan("tiny"))
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Server running on PORT ${PORT}`)
+})
+
