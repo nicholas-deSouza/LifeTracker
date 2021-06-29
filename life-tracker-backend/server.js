01ce747643ba8723f53dbcd73ccce3e6442323f2
Nicholas de Souza <nrdesouzag13@gmail.com>
2021-06-29 09:36:07 -0700

    db created

--- life-tracker-backend/server.js
+++ life-tracker-backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const cors = require("cors")
 const morgan = require("morgan")
+const {BadRequestError, NotFoundError} = require("./utils/error")
 
 const app = express()
 
@@ -10,6 +11,20 @@ app.use(cors())
 app.use(express.json())
 
 app.use(morgan("tiny"))
+
+app.use((req,res,next) => {
+    return next (new NotFoundError())
+})
+
+app.use((err, req,res,next) => {
+    const status = err.status || 500
+    const message = err.message
+
+    return res.status(status).json({
+        error: {message, status}
+    })
+        
+})
 
 const PORT = process.env.PORT || 3001
 
